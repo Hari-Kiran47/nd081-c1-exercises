9bf35dde0149a0483467df07c8173bbc1cdf06ac
Hari-Kiran47 <118198220+Hari-Kiran47@users.noreply.github.com>
2025-08-23 18:42:25 +0530

    Update config.py

--- L3. Azure Storage/Connecting_to_your_App_to_Storage/config.py
+++ L3. Azure Storage/Connecting_to_your_App_to_Storage/config.py
@@ -7,10 +7,10 @@ class Config(object):
     SQL_SERVER = os.environ.get('SQL_SERVER') or 'hello-world1234.database.windows.net'
     SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'hello-world-db'
     SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or 'harikiranadmin '
-    SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or 'H@rikiran9030'
+    SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or ''
     SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://' + SQL_USER_NAME + '@' + SQL_SERVER + ':' + SQL_PASSWORD + '@' + SQL_SERVER + ':1433/' + SQL_DATABASE + '?driver=ODBC+Driver+17+for+SQL+Server'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT') or 'helloworld1234b'
-    BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY') or '7svW2Am2UkDImM4ks/HDOU+7Yxz+Lwo7J0UC1fyNwu3xUvkXIBuVxXTv9UsAvhzJTApzmvBPcWp++AStj+BT5A=='
+    BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY') or ''
     BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER') or 'images'
